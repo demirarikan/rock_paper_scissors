88eee00962f4c5e18eb088aecb2affe64dc1ca67
Demir <dmrarikan@gmail.com>
2022-03-14 18:42:51 +0100

    added ability to play a game of 5 rounds

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function computerPlay(){
     let result = Math.floor(Math.random() * 3)
-    console.log(result)
     if(result == 0) return "Rock"
     if(result == 1) return "Paper"
     else return "Scissor"
@@ -9,6 +8,8 @@ function computerPlay(){
 function playRound(playerSelection, computerSelection){
     let player = playerSelection.toLowerCase()
     let cpu = computerSelection.toLowerCase()
+    console.log(`Player: ${player}`)
+    console.log(`Cpu: ${cpu}`)
     if(player === cpu) return "It's a tie!"
     if(player === "rock" && cpu === "paper") return `CPU wins, ${cpu} beats ${player}`
     if(player === "rock" && cpu === "scissor") return `Player wins, ${player} beats ${cpu}`
@@ -19,14 +20,22 @@ function playRound(playerSelection, computerSelection){
 }
 
 function game() {
+    let gameResult = 0
     for(let i=0; i<5; i++){
-        console.log(playRound(window.prompt("Rock, paper or scissor?"), computerPlay()))
-        
+        let roundResult = playRound(window.prompt("Rock, paper or scissor?"), computerPlay())
+        console.log(roundResult[0])
+        if(roundResult[0]==="P") gameResult++
+        if(roundResult[0]==="C") gameResult--
+        console.log(roundResult)
+        console.log(gameResult)
     }
+    if(gameResult<0) return "CPU wins"
+    if(gameResult>0) return "Player wins"
+    else return "It's a tie"
 }
 
-const playerSelection = "ROck"
-let computerSelection = computerPlay()
-console.log(computerSelection)
-console.log(playRound(playerSelection, computerSelection))
+// const playerSelection = "ROck"
+// let computerSelection = computerPlay()
+// console.log(computerSelection)
+// console.log(playRound(playerSelection, computerSelection))
 
