f57c2abb5fc361d1a1124a9cf3f1539b263de848
Demir <dmrarikan@gmail.com>
2022-03-14 18:10:50 +0100

    added function to calculate results of 1 round

--- script.js
+++ script.js
@@ -10,4 +10,23 @@ function playRound(playerSelection, computerSelection){
     let player = playerSelection.toLowerCase()
     let cpu = computerSelection.toLowerCase()
     if(player === cpu) return "It's a tie!"
+    if(player === "rock" && cpu === "paper") return `CPU wins, ${cpu} beats ${player}`
+    if(player === "rock" && cpu === "scissor") return `Player wins, ${player} beats ${cpu}`
+    if(player === "paper" && cpu === "rock") return `Player wins, ${player} beats ${cpu}`
+    if(player === "paper" && cpu === "scissor") return `CPU wins, ${cpu} beats ${player}`
+    if(player === "scissor" && cpu === "rock") return `CPU wins, ${cpu} beats ${player}`
+    if(player === "scissor" && cpu === "paper") return `Player wins, ${player} beats ${cpu}`
 }
+
+function game() {
+    for(let i=0; i<5; i++){
+        console.log(playRound(window.prompt("Rock, paper or scissor?"), computerPlay()))
+        
+    }
+}
+
+const playerSelection = "ROck"
+let computerSelection = computerPlay()
+console.log(computerSelection)
+console.log(playRound(playerSelection, computerSelection))
+
